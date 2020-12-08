c650fa4c44b91fdca3ec356c6049fcb31fe4d61a
PupRiku <chris.diorio12@gmail.com>
2020-12-08 09:15:40 -0600

    Add Message arrays. Print arrays to test

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Store Message components
+const food = ["chicken and potaotes", "burrito bowl", "pizza", "pasta with meat sauce", "mac and cheese", "steak and salad"];
+const chores = ["laundry", "clean bathroom", "clean bedroom", "clean living room", "clean kitchen", "vaccum", "trash & recycling", "dusting", "dishes"];
+const games = ["Kingdom Hearts", "Destiny 2", "Danganronpa", "Minecraft", "Final Fantasy", "Fire Emblem"];
 
 // Create message
 
 // Run program
+console.log(food);
+console.log(chores);
+console.log(games);
